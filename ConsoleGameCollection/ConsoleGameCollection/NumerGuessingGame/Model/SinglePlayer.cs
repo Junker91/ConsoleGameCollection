112cf3a7a0432d20e8b3abb8f2ba7c3d89557558
Xena92 <Ida.thuesen@hotmail.com>
2014-06-28 20:13:29 +0200

    Added new gamemodes and players for guessing game

--- ConsoleGameCollection/ConsoleGameCollection/NumerGuessingGame/Model/SinglePlayer.cs
+++ ConsoleGameCollection/ConsoleGameCollection/NumerGuessingGame/Model/SinglePlayer.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleGameCollection.NumerGuessingGame.Model {
-    class NumberGuessingGame {
-        //Calculation and logistics for the guessing game
+    class SinglePlayer {
+        
     }
 }
