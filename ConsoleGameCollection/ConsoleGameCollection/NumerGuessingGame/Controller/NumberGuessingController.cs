92d7e142971cde3c48626ea01b2d9ded87fab4ec
Junker91 <sashiba91@gmail.com>
2014-06-28 19:53:37 +0200

    Fixies

--- ConsoleGameCollection/ConsoleGameCollection/NumerGuessingGame/Controller/NumberGuessingController.cs
+++ ConsoleGameCollection/ConsoleGameCollection/NumerGuessingGame/Controller/NumberGuessingController.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleGameCollection {
-    interface IView {
-        void menu();
-
+namespace ConsoleGameCollection.NumerGuessingGame.Controller {
+    class NumberGuessingController {
     }
 }
