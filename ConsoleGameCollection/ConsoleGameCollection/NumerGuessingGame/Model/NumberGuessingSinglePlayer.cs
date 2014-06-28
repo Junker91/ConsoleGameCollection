4396092aaadf37a593dea102d778cf25de27f27b
Xena92 <Ida.thuesen@hotmail.com>
2014-06-28 22:12:11 +0200

    Finishing touches on StartGame()

--- ConsoleGameCollection/ConsoleGameCollection/NumerGuessingGame/Model/NumberGuessingSinglePlayer.cs
+++ ConsoleGameCollection/ConsoleGameCollection/NumerGuessingGame/Model/NumberGuessingSinglePlayer.cs
@@ -9,27 +9,34 @@ namespace ConsoleGameCollection.NumerGuessingGame.Model {
         private Player _player;
         private int _minimunValue;
         private int _maximunValue;
-        private int _correctValue;
+        
 
+        /// <param name="player">The name of the player </param>
+        /// <param name="minimunValue">The minimum value of the interval that should be guessed in</param>
+        /// <param name="maximunValue">The maximum value of the interval that should be guessed in</param>
         public NumberGuessingSinglePlayer(Player player, int minimunValue, int maximunValue) {
             this._player = player;
             this._minimunValue = minimunValue;
             this._maximunValue = maximunValue;
+            Random randNum = new Random();
 
+            //Adds a value (the number that should be found) in the dictionary on the players space. Becomes more useful in multiplayer game.
+            base._correctValueDic.Add(_player.Name, randNum.Next(_minimunValue, _maximunValue));
 
             StartGame();
         }
 
+        //Method that plays out the singleplayer game. For each try it removes a life, until there are no more lives.
         public void StartGame() {
             bool gameWon = false;
             int input;
 
-            Random randNum = new Random();
-            _correctValue = randNum.Next(_minimunValue, _maximunValue);
+            int _correctValue = base._correctValueDic[_player.Name];
 
             Console.WriteLine("Welcome to the guessing game!");
             Console.WriteLine("You have {0} tries to find the right number between {1} and {2}", _player.Lives, _maximunValue, _maximunValue);
 
+            //Keeps running until the player either wins or loses the game
             do{
                 Console.WriteLine("Take a guess");
                 input = int.Parse(Console.ReadLine());
@@ -51,14 +58,13 @@ namespace ConsoleGameCollection.NumerGuessingGame.Model {
             } while (!gameWon && _player.Lives > 0);
 
             if(_player.Lives <= 0){
-                Console.WriteLine("You did not find the number within the timeline, better luck next time\n" +
-                                  "Press any key to continue.");
+                Console.WriteLine("You did not find the number within the timeline. The number was {0}, better luck next time\n" +
+                                  "Press any key to continue.", _correctValue);
             }
             else {
-                Console.WriteLine("Congratulations, you won!\n" +
-                                  "Press any key to continue");
+                Console.WriteLine("Congratulations you found the number {0} and won the game!\n" +
+                                  "Press any key to continue", _correctValue);
             }
-
             Console.ReadKey();
                 
 
