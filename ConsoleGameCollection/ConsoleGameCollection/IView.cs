9ce1eb7fb2e87e783138ffa126420956680b318e
Junker91 <sashiba91@gmail.com>
2014-06-28 19:59:24 +0200

    View changes

--- ConsoleGameCollection/ConsoleGameCollection/IView.cs
+++ ConsoleGameCollection/ConsoleGameCollection/IView.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleGameCollection {
     interface IView {
-        void menu();
-
+        void Menu();
+        void DrawGame();
     }
 }
--- ConsoleGameCollection/ConsoleGameCollection/NumerGuessingGame/View/NumberGuessingView.cs
+++ ConsoleGameCollection/ConsoleGameCollection/NumerGuessingGame/View/NumberGuessingView.cs
@@ -5,6 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleGameCollection.NumerGuessingGame.View {
-    class NumberGuessingView {
+    class NumberGuessingView : IView {
+        // Draws everything to the screen. Communicates with the user
+
+        public void Menu() {
+
+        }
+
+        public void DrawGame() {
+
+        }
     }
 }
