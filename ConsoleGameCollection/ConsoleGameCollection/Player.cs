b9376a8d5889d1a6f827b659af08df2e0baef2b1
Junker91 <junker-91@hotmail.com>
2014-06-28 19:40:56 +0200

    player class

---
+++ ConsoleGameCollection/ConsoleGameCollection/Player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGameCollection {
+    public abstract class Player {
+        protected int score;
+    }
+}
