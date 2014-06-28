ae99066b9174c7a85e5207f0fad927780aa1c840
Junker91 <junker-91@hotmail.com>
2014-06-28 19:49:03 +0200

    player is implemented
    name, score and lives have been added

--- ConsoleGameCollection/ConsoleGameCollection/Player.cs
+++ ConsoleGameCollection/ConsoleGameCollection/Player.cs
@@ -6,6 +6,27 @@ using System.Threading.Tasks;
 
 namespace ConsoleGameCollection {
     public abstract class Player {
+        protected string name;
         protected int score;
+        protected int lives;
+
+
+        public virtual string Name {
+            get {
+                return name;
+            }
+            set {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("name string is null or empty");
+
+                else
+                    name = value;
+                
+            }
+        }
+
+        public abstract int Score { get; set; }
+
+        public abstract int lives { get; set; }
     }
 }
