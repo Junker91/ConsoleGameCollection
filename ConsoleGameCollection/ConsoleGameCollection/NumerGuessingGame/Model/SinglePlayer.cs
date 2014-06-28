b6c92079d0e909f162c4619f05639d977148dd79
Xena92 <Ida.thuesen@hotmail.com>
2014-06-28 20:43:46 +0200

    Changes in singleplayer

--- ConsoleGameCollection/ConsoleGameCollection/NumerGuessingGame/Model/SinglePlayer.cs
+++ ConsoleGameCollection/ConsoleGameCollection/NumerGuessingGame/Model/SinglePlayer.cs
@@ -5,7 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleGameCollection.NumerGuessingGame.Model {
-    class SinglePlayer {
-        
+    public class SinglePlayer {
+        private Player _player;
+        private int _minimunValue;
+        private int _maximunValue;
+
+        public SinglePlayer(Player player, int minimunValue, int maximunValue) {
+            this._player = player;
+            this._minimunValue = minimunValue;
+            this._maximunValue = maximunValue;
+        }
     }
 }
